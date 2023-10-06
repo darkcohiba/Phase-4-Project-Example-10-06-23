9a23a9aa46df61d01c8cfa519891b2c27283a897
Sam Waters <samuel.waters@gmail.com>
2023-10-06 15:43:37 -0600

    feat: created models, fighter route and seeds

--- server/app.py
+++ server/app.py
@@ -9,6 +9,8 @@ from flask_restful import Resource
 # Local imports
 from config import app, db, api
 # Add your model imports
+from models import db, Gladiator, Arena, Fights
+
 
 
 # Views go here!
@@ -18,6 +20,33 @@ def index():
     return '<h1>Phase 4 Project Server</h1>'
 
 
+class Fighters(Resource):
+        def get(self):
+            fighters = [f.to_dict(rules=('-arena_field.fight_field', '-gladiator_field')) for f in Fights.query.all()]
+            return fighters, 200
+        def post(self):
+            request_obj = request.get_json()
+            try:
+                print(request_obj["gladiator"], request_obj["arena"])
+                new_fight = Fights(
+                    gladiator_id =request_obj["gladiator"],
+                    arena_id = request_obj["arena"],
+                )
+                db.session.add(new_fight)
+                db.session.commit()
+
+            except Exception as e:
+                message = {'errors': [e.__str__()]}
+                return message, 422
+            
+            return new_fight.to_dict(),200
+        
+
+api.add_resource(Fighters, '/fighters')
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+
+
+
 
--- server/seed.py
+++ server/seed.py
@@ -8,10 +8,51 @@ from faker import Faker
 
 # Local imports
 from app import app
-from models import db
+from models import db, Gladiator, Arena, Fights
 
 if __name__ == '__main__':
     fake = Faker()
     with app.app_context():
         print("Starting seed...")
+        # db.create_all()
+
+        # delete my previous data
+        db.session.query(Gladiator).delete()
+        db.session.query(Arena).delete()
+        
+
+
+        print("seeding gladiators")
+        g1 = Gladiator(name="Ryan", number_of_fights=25, weapon="Shiv")
+        g2 = Gladiator(name="Jake", number_of_fights=29, weapon="Bare Hands")
+        g3 = Gladiator(name="Cristal", number_of_fights=50, weapon="Nails")
+        glads= [g1,g2,g3]
+        db.session.add_all(glads)
+        db.session.commit()
+
+        print("seeding arenas")
+
+
+        a1 = Arena(title="Worchestire", location="France")
+        a2 = Arena(title="A Random Divet on Mars", location="Mars")
+        a3 = Arena(title="Mr.Beast's Private Island", location="Need to Know")
+        arenas = [a1, a2, a3]
+        db.session.add_all(arenas)
+        db.session.commit()
+
+        print("seeding fights")
+        fights = []
+        for arena in arenas:
+            f1 = Fights(gladiator_id=rc(glads).id, arena_id=arena.id)
+            fights.append(f1)
+        db.session.add_all(fights)
+        db.session.commit()
+
+
+
+
+
+
+
+        # db.create_all()
         # Seed code goes here!
